739e695f20b459dcba13b1b21364a6b58d434565
misterkisus <88712927+misterkisus@users.noreply.github.com>
2023-03-21 11:22:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,70 @@
 const generateButton = document.getElementById('generateButton');
 
 function animateText(element) {
-    element.classList.add('fade-in');
-    setTimeout(function () {
-        element.classList.remove('fade-in');
-    }, 800);
+  element.classList.add('fade-in');
+  setTimeout(function () {
+    element.classList.remove('fade-in');
+  }, 800);
 }
 
-function startCountdown(firstClick) {
-    const now = new Date().getTime();
-    const nextClickTime = now + 24 * 60 * 60 * 1000;
-    localStorage.setItem('nextClickTime', nextClickTime);
-    if (!firstClick) {
-        updateCountdown(nextClickTime);
-    }
+function startCountdown() {
+  const now = new Date().getTime();
+  const nextClickTime = now + 24 * 60 * 60 * 1000;
+  localStorage.setItem('nextClickTime', nextClickTime);
+  updateCountdown(nextClickTime);
 }
 
 function updateCountdown(nextClickTime) {
-    const now = new Date().getTime();
-    const timeLeft = nextClickTime - now;
+  const now = new Date().getTime();
+  const timeLeft = nextClickTime - now;
 
-    if (timeLeft <= 0) {
-        enableButton();
-        return;
-    }
+  if (timeLeft <= 0) {
+    localStorage.removeItem('nextClickTime');
+    enableButton();
+    return;
+  }
 
-    const hours = Math.floor(timeLeft / (1000 * 60 * 60));
-    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  const hours = Math.floor(timeLeft / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    generateButton.innerHTML = `Осталось: ${hours}ч ${minutes}м ${seconds}с`;
-    setTimeout(() => updateCountdown(nextClickTime), 1000);
+  generateButton.innerHTML = `Осталось: ${hours}ч ${minutes}м ${seconds}с`;
+  setTimeout(() => updateCountdown(nextClickTime), 1000);
 }
 
 function disableButton() {
-    generateButton.disabled = true;
-    generateButton.style.opacity = 0.5;
-    startCountdown(false);
+  generateButton.disabled = true;
+  generateButton.style.opacity = 0.5;
+  startCountdown();
 }
 
 function enableButton() {
-    generateButton.disabled = false;
-    generateButton.style.opacity = 1;
-    generateButton.innerHTML = 'Кто сегодня Ваня';
+  generateButton.disabled = false;
+  generateButton.style.opacity = 1;
+  generateButton.innerHTML = 'Кто сегодня Ваня';
 }
 
-generateButton.addEventListener('click', function() {
-    const phrases = ["казел", "казлина", "говнюк"];
-    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-    const heading = document.querySelector('h1');
-    heading.innerHTML = `Ваня сегодня ${randomPhrase}`;
-    animateText(heading);
-    disableButton();
+generateButton.addEventListener('click', function () {
+  const phrases = ['казел', 'казлина', 'говнюк'];
+  const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+  const heading = document.querySelector('h1');
+  heading.innerHTML = `Ваня сегодня ${randomPhrase}`;
+  animateText(heading);
+  disableButton();
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const nextClickTime = localStorage.getItem('nextClickTime');
+// Остальной код остается без изменений
 
-    if (!nextClickTime) {
+document.addEventListener('DOMContentLoaded', function () {
+    // Добавим задержку перед проверкой состояния таймера в localStorage
+    setTimeout(() => {
+      const nextClickTime = localStorage.getItem('nextClickTime');
+
+      if (!nextClickTime || new Date().getTime() >= nextClickTime) {
         enableButton();
-        startCountdown(true);
-    } else if (new Date().getTime() >= nextClickTime) {
-        enableButton();
-    } else {
+      } else {
         disableButton();
         updateCountdown(parseInt(nextClickTime, 10));
-    }
-});
+      }
+    }, 100); // Задержка в 100 миллисекунд
+  });
